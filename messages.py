632954926f9acdd20789eaadbd5815fb96dd87be
kylobyne <kylobyne@gmail.com>
2026-07-12 20:31:55 +0400

    исправления текста

--- messages.py
+++ messages.py
@@ -26,7 +26,7 @@ COOLDOWN_EARLY = '<i>{name}, повтори через {minutes} мин. {second
 
 EMPTY_LEADERBOARD = '<tg-emoji emoji-id="5264737672684907396">🍻</tg-emoji> Пока никто в этом чате не пил пиво. Используйте /beer первым!'
 
-LEADERBOARD_TITLE = '<b><i><tg-emoji emoji-id="5409008750893734809">🏆</tg-emoji> Топ игроков чата — страница</i></b> ({page}/{pages}):\n'
+LEADERBOARD_TITLE = '<b><i><tg-emoji emoji-id="5409008750893734809">🏆</tg-emoji> Топ игроков чата</i></b> (стр. {page}/{pages}):\n'
 LEADERBOARD_ROW = "{number}. <i>{name} — <b>{liters} л.</b></i>"
 TEXT_INFO = "\n\n<i>Чтобы попасть в этот список, начните игру с помощью команды /beer</i>"
 
