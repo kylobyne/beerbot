e55160e3c6adfb78f570e1a29a5d0ac8e6fb39c3
kylobyne <kylobyne@gmail.com>
2026-07-14 18:33:57 +0400

    add BOTNAME in messages.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ if not TOKEN:
     raise RuntimeError("Задайте BOT_TOKEN в файле .env")
 
 
+
 COOLDOWN_SECONDS = 60 * 60
 ROWS_PER_PAGE = 20
 PAGINATION_COOLDOWN_SECONDS = 3
-
 
 DATABASE_DIRECTORY = Path("sqlite3")
 DATABASE_DIRECTORY.mkdir(exist_ok=True)
